118b78891728100bca66d2039eaee95de45f4b52
GuangMing546 <1919595638@qq.com>
2020-11-20 21:27:55 +0800

    init commit

---
+++ src/main/java/com/wyu/ibm_v2/IbmV2Application.java
@@ -0,0 +1,13 @@
+package com.wyu.ibm_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbmV2Application.class, args);
+    }
+
+}
